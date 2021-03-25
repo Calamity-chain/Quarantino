772f5ac781ee1f519e72be2dcf2e15fbb540191d
Calamity-chain <78163718+Calamity-chain@users.noreply.github.com>
2021-03-25 22:03:47 +0100

    Updated allowed origins with cors in index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-
-const cors = require('cors');
-app.use(cors());
-
-// TO CREATE A LIST OF ALLOWED DOMAINS use this :
-
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ //If a specific origin isn't found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn\'t allow access from origin' + origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
-
-
 const express = require("express");
 (morgan = require("morgan")),
   (bodyParser = require("body-parser")),
@@ -25,6 +5,23 @@ const express = require("express");
   (mongoose = require('mongoose')),
   (passport = require('passport'),
               require('./passport'));
+
+const cors = require('cors');
+
+// TO CREATE A LIST OF ALLOWED DOMAINS use this :
+
+let allowedOrigins = ['http://localhost:8080', 'https://quarantinoflix.herokuapp.com/'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ //If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 const { check, validationResult } = require('express-validator');  
 
